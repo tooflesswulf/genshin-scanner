1fb47aba4970e65ceb5492b791156db4bd6492aa
tooflesswulf <tooflesswulf0325@gmail.com>
2023-12-19 21:28:19 -0500

    initial commit

---
+++ dataset.py
@@ -0,0 +1,78 @@
+import torch
+from torch.utils.data import Dataset
+import os
+import json
+from PIL import Image
+import numpy as np
+
+import torchvision.transforms as tr
+from torchvision.transforms.functional import crop, resized_crop
+from torchvision.datasets import VisionDataset
+
+
+class GenshinArtifactDataset(Dataset):
+    def __init__(self, root_dir, transform=None, min_bbox_overlap=.95, im_size=100):
+        self.root = root_dir
+        self.transform = transform
+        self.min_bbox_overlap = min_bbox_overlap
+        self.im_size = im_size
+
+        # static db
+        self.image_files = [f'image{i}.png' for i in range(10)]
+        self.anno_files = [f'image{i}-anno.json' for i in range(10)]
+
+        self.images = [Image.open(os.path.join(root_dir, im)).convert('RGB')
+                       for im in self.image_files]
+        self.annotations = [json.load(open(os.path.join(root_dir, af), 'r'))
+                            for af in self.anno_files]
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, ix):
+        im = self.images[ix]
+        anno = self.annotations[ix]
+
+        # Produce random bbox with some minimum overlap.
+        bbox = anno['bbox']
+        x1, x2, y1, y2 = bbox
+        wigx = (1 - self.min_bbox_overlap)*(x2 - x1)
+        wigy = (1 - self.min_bbox_overlap)*(y2 - y1)
+        a, b, c, d = bbox + np.array([wigx, wigy, -wigx, -wigy])
+
+        z = np.array([-1, -1, 1, 1])
+        orig_area = np.prod(bbox @ np.array([[-1, 0, 1, 0], [0, -1, 0, 1]]).T)
+
+        rand_crop = np.random.uniform([0, 0, c, d], [a, b, 1, 1])
+        aoi = np.prod(np.amin([z*bbox, z*rand_crop], axis=0)
+                      @ np.array([[1, 0, 1, 0], [0, 1, 0, 1]]).T)
+        while aoi / orig_area < self.min_bbox_overlap:
+            rand_crop = np.random.uniform([0, 0, c, d], [a, b, 1, 1])
+            aoi = np.prod(np.amin([z*bbox, z*rand_crop], axis=0)
+                          @ np.array([[1, 0, 1, 0], [0, 1, 0, 1]]).T)
+
+        # Use obtained random crop to transform image & boxes.
+        x1, y1, x2, y2 = (np.array([*im.size, *im.size]) * rand_crop).astype(int)
+        im = resized_crop(im, y1, x1, y2-y1, x2-x1, [self.im_size, self.im_size])
+        anno2 = {}
+        fields = ['title', 'slot', 'mainstat', 'level',
+                  'rarity', 'substat', 'set', 'lock', 'bbox']
+
+        _a = np.array(rand_crop[:2])
+        _b = np.array(rand_crop[2:])
+        a = np.array([*_a, *_a])
+        b = np.array([*(_b-_a), *(_b-_a)])
+        for f in fields:
+            anno2[f] = (anno[f] - a) / b
+
+        y_regr = []
+        for f in fields:
+            y_regr.extend(anno2[f])
+
+        if self.transform is not None:
+            im = self.transform(im)
+        return {
+            'image': im,
+            'y1': torch.Tensor(y_regr),
+            **anno2
+        }
---
+++ training.py
@@ -0,0 +1,82 @@
+import dataset
+import torchvision.transforms.v2 as v2
+import torchvision
+import PIL
+
+import torch
+import numpy as np
+
+from torch import nn
+from torch.utils.data import DataLoader, RandomSampler
+
+from pytorch_lightning import LightningModule, Trainer
+
+
+class SimpleNetwork(nn.Module):
+    def __init__(self, im_size=100, out_size=36):
+        super(SimpleNetwork, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(3, 32, 5),  # h-4, w-4
+            nn.ReLU(),
+            nn.MaxPool2d(4, stride=4),  # h/2, w/2
+            nn.Conv2d(32, 64, 5),  # h-4, w-4
+            nn.ReLU(),
+            nn.MaxPool2d(4, stride=4),  # h/2, w/2
+        )
+
+        self.conv_size = im_size
+        for _ in range(2):
+            self.conv_size = (self.conv_size - 4) // 4
+        self.fc_size = 64 * self.conv_size * self.conv_size
+        self.fc = nn.Sequential(
+            nn.Linear(self.fc_size, 64),
+            nn.ReLU(),
+            nn.Linear(64, out_size),
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.view(-1, self.fc_size)
+        x = self.fc(x)
+        return x
+
+
+class LModule(LightningModule):
+    def __init__(self, model: nn.Module, lr=1e-5):
+        super(LModule, self).__init__()
+        self.model = model
+        self.loss = nn.MSELoss()
+        self.lr = lr
+
+    def configure_optimizers(self):
+        opt = torch.optim.Adam(self.parameters(), lr=self.lr)
+        return opt
+
+    def training_step(self, batch, bi):
+        x = batch['image']
+        y = batch['y1']
+
+        loss = self.loss(self.model(x), y)
+        self.log('train_loss', loss, on_epoch=True, batch_size=x.shape[0])
+        return loss
+
+if __name__ == '__main__':
+    tr = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.uint8, scale=True),
+    v2.ColorJitter(brightness=.3, hue=.05, contrast=.1),
+    v2.ToDtype(torch.float32, scale=True),  # Normalize expects float input
+    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Imagenet normalization
+    ])
+
+    im_size = 128
+
+    ds = dataset.GenshinArtifactDataset('/Users/albertxu/Desktop/go-tests/scanner-data/', im_size=im_size, transform=tr)
+    samp = RandomSampler(ds, replacement=True, num_samples=640)
+    loader = DataLoader(ds, batch_size=64, sampler=samp)
+
+    model = SimpleNetwork(im_size=im_size)
+    m = LModule(model, lr=1e-5)
+    trainer = Trainer(max_epochs=10_000, accelerator='gpu')
+    trainer.fit(m, loader)
+
