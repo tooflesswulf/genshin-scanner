e534428b5ea68f6725cc3ad95823852b0f45d8bc
tooflesswulf <tooflesswulf0325@gmail.com>
2023-12-24 23:30:34 -0800

    torch2tf code

---
+++ torch2tf.py
@@ -0,0 +1,33 @@
+# from onnx_tf.backend import prepare
+import torch
+import sys
+
+import nobuco
+from nobuco import ChannelOrder, ChannelOrderingStrategy
+from nobuco.layers.weight import WeightLayer
+
+from training import SimpleNetwork, LModule
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print(f'Usage: {sys.argv[0]} /path/to/checkpoint.ckpt')
+        exit(0)
+
+    model_path = sys.argv[1]
+    print(f'Converting {model_path} from torch to TF.')
+    m = LModule(SimpleNetwork(im_size=200))
+    ckpt = torch.load(model_path, map_location=torch.device('cpu'))
+    m.load_state_dict(ckpt['state_dict'])
+    m.eval()
+
+    dummy_input = torch.zeros((1, 3, 200, 200))
+    print(m.model(dummy_input))
+
+    keras_model = nobuco.pytorch_to_keras(
+        m.model,
+        args=[dummy_input], kwargs=None,
+        inputs_channel_order=ChannelOrder.TENSORFLOW,
+        outputs_channel_order=ChannelOrder.TENSORFLOW
+    )
+    print(keras_model)
