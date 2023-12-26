940a38db3384e1de2edf266dc8d8799ccdaa53b3
tooflesswulf <tooflesswulf0325@gmail.com>
2023-12-26 12:13:59 -0800

    onnx

---
+++ torch2onnx.py
@@ -0,0 +1,42 @@
+# from onnx_tf.backend import prepare
+import torch
+import torch.onnx
+import sys
+
+# import nobuco
+# from nobuco import ChannelOrder, ChannelOrderingStrategy
+# from nobuco.layers.weight import WeightLayer
+# import tensorflow as tf
+# import tensorflowjs as tfjs
+
+from training import SimpleNetwork, LModule
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print(f'Usage: {sys.argv[0]} /path/to/checkpoint.ckpt')
+        exit(0)
+
+    model_path = sys.argv[1]
+    print(f'Converting {model_path} from torch to TF.')
+    m = LModule(SimpleNetwork(im_size=200))
+    ckpt = torch.load(model_path, map_location=torch.device('cpu'))
+    m.load_state_dict(ckpt['state_dict'])
+    m.eval()
+
+    dummy_input = torch.zeros((1, 3, 200, 200))
+    print(m.model(dummy_input))
+
+    # torch.onnx.export(m.model, )
+    torch.onnx.export(m.model, dummy_input, "onnx/simplenet.onnx", verbose=True, input_names=['input1'], output_names=['output1'])
+
+    # keras_model = nobuco.pytorch_to_keras(
+    #     m.model,
+    #     args=[dummy_input], kwargs=None,
+    #     inputs_channel_order=ChannelOrder.TENSORFLOW,
+    #     outputs_channel_order=ChannelOrder.TENSORFLOW
+    # )
+    # print(keras_model(tf.zeros((1, 200, 200, 3))))
+
+    # tfjs.converters.save_keras_model(keras_model, 'tfjs')
+
